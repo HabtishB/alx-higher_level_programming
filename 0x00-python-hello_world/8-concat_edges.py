9c6f2acbc76a5a1e2b44f93ccba70556034a61d3
HabtishB <behabtm@gmail.com>
2021-11-16 21:25:01 +0000

    files added

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntaxn"
+str = str[39:66] + str[110:114] + str [:6]
+print(str)
