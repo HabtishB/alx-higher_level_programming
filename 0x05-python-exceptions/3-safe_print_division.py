63c21d734015ad6c80f31c6f8fb89e22db34c8ea
HabtishB <behabtm@gmail.com>
2021-12-06 19:59:50 +0000

    3 safe print added

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+    except (ZeroDivisionError, TypeError):
+        result = None
+    finally:
         print("Inside result: {}".format(result))
-        return result
-    except ZeroDivisionError:
-        print("Inside result: {}".format(None))
-        return None
+    return result
