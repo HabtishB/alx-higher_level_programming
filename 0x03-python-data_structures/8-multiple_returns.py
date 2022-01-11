bfe0eaa9a0a9cecb3a8be99ddc4266b41de73e3b
HabtishB <behabtm@gmail.com>
2022-01-11 09:59:59 -0800

    8 modified

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,4 @@ def multiple_returns(sentence):
     if sentence is not None:
         if len(sentence) == 0:
             return (0, None)
-        else:
-            return(len(sentence), sentence[0])
+        return(len(sentence), sentence[0])
