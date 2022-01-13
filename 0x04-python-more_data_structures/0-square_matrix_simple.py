1232346e5c163830a06f1abdadaefc6ad8ff0fdd
HabtishB <behabtm@gmail.com>
2022-01-13 10:36:45 -0800

    0-square added

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_a =[]
+    matrix_a = []
     for row in matrix:
         list_a = []
         for column in row:
-            list_a.append(column **2)
+            list_a.append(column ** 2)
         matrix_a.append(list_a)
     return matrix_a
