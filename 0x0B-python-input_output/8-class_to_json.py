3f90097b04b3d21b323bac704893e9a5d6d9a679
HabtishB <behabtm@gmail.com>
2022-02-08 19:16:28 -0800

    8 modified

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,9 +6,6 @@ import json
 def class_to_json(obj):
     """ A function that returns the dictionary description with simple
     data structures like (list, dictionary, strion, integer and boolean)
-    for JSON serialization of an object 
-    """
-    data = {}
-    for attr in obj.__dict__:
-        data[attr] = obj.__dict__[attr]
-    return data
+    for JSON serialization of an object """
+
+    return obj.__dict__
