b2a8dd49ff7fed358139509125ae15e3a2d86941
HabtishB <behabtm@gmail.com>
2022-02-09 17:37:29 -0800

    add attribute added

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, attribute, value):
     """ This method adds a new attribute to an object """
 
-    if '__dict__' not in dir(obj): 
+    if '__dict__' not in dir(obj):
         raise TypeError("can't add a new attribute")
     if '__slots__' in dir(obj):
         raise TypeError("can't add new attribute")
