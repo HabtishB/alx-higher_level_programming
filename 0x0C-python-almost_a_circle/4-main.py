f1264afb08d7274ea971610dbf65ad0e36f6e994
HabtishB <behabtm@gmail.com>
2022-02-14 13:25:09 -0800

    python almost a circle added

---
+++ 0x0C-python-almost_a_circle/4-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" 0-main """
+
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+    r1 = Rectangle(3, 4)
+    r1.display()
+
+    print("------")
+
+    r2 = Rectangle(2, 8)
+    r2.display()
+    
