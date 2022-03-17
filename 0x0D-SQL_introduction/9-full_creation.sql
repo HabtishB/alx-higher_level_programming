3d9448e5e30d4c22dc4bef73b25507ded51efca1
HabtishB <behabtm@gmail.com>
2022-03-17 10:03:59 -0700

    full creation modified

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 -- script creates table second table in the database
 
-CREATE TABLE IF NOT EXISTS second_table(
+USE hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (
      id INT,
      name VARCHAR(256),
      score INT
