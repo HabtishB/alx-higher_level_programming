29127b1ff0cd4170bfa3de338638bf042a16fb2e
HabtishB <behabtm@gmail.com>
2022-03-17 17:58:28 -0700

    100 added

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- list all genres not linked to the show Dexter
+
+SELECT tg.name
+  FROM tv_genres AS tg
+  WHERE tg.id NOT IN
+       ( SELECT tg.id
+         FROM tv_genres AS tg
+	 INNER JOIN tv_show_genres AS tsg
+	 ON tg.id = tsg.genre_id
+	 INNER JOIN tv_shows AS ts
+	 ON tsg.show_id = ts.id
+	 WHERE ts.title = "Dexter")
+  ORDER BY tg.name;
