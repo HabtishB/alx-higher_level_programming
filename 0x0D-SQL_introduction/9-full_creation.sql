03bda6c29871d5911e1d23d26a83a343cd4f5e4f
HabtishB <behabtm@gmail.com>
2022-03-17 18:06:15 -0700

    9 full creation updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- script creates table second table in the database
+-- creates table second table in the database
 
 USE hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS second_table (
      score INT
 );
 
-INSERT INTO second_table(id, name, score)
-VALUES ROW(1, "JOHN", 10),
+INSERT INTO second_table (id, name, score)
+VALUES ROW(1, "John", 10),
        ROW(2, "Alex", 3),
        ROW(3, "Bob", 14),
        ROW(4, "George", 8);
