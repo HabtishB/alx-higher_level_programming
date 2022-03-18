730f9c0b70d168a49c774c20ed8917f6c7b456eb
HabtishB <behabtm@gmail.com>
2022-03-17 17:15:37 -0700

    3-force_name modified

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,11 @@
 -- creates the table force_name on your MySQL server.
   -- force_name description:
-    --id INT
+    -- id INT
     -- name VARCHAR(256)
   -- The database name will be passed as an argument of the mysql command
   -- if the table exists, it shouldn't fail
 
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+       id INT,
+       name VARCHAR(256) NOT NULL
+);
