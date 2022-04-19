35cff3ce7b91e1a5aba2feafbcb8db9a8df9e243
HabtishB <behabtm@gmail.com>
2022-04-19 08:56:45 -0700

    2-my_filter_states are added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
 """
+This script takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa
+It takes 3 arguments username, password, and database name
+It runs on a localhost at port 3306
+Results are displayed in ascending ordering using states.id
 """
 
 import sys
@@ -9,9 +13,11 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3], host='localhost', port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states\
+                    WHERE name LIKE BINARY '{}'\
+                    ORDER BY states.id ASC".format(sys.argv[4]))
 
     states = cursor.fetchall()
     for state in states:
