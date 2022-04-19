4004139cb632b0d3f24bd90d3ccf99c2ffd8a148
HabtishB <behabtm@gmail.com>
2022-04-19 09:31:28 -0700

    3-my_safe_filter_states.py added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-
 """
+This script is also the same as the first one that takes in arguments
+and displays all values in the states of table hbtn_0e_0_usa where
+the names matches the argument. But this script is safe from MySQL
+injection becauase of the bound paramaters.
+It takes four arguments, username, password, dbname and statename
+(The searched paramater)
+The script connects to the MySQL database server on localhost at
+port 3306
+Results are displayed on ascending order by states.id
 """
 
 import sys
@@ -9,9 +17,11 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3], host="localhost", port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %(state_name)s ORDER BY states.id ASC", {'state_name':sys.argv[4]})
+    cursor.execute("SELECT * FROM states\
+                   WHERE name = %(state_name)s\
+                   ORDER BY states.id ASC", {'state_name': sys.argv[4]})
 
     states = cursor.fetchall()
     for state in states:
