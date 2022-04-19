45e4ca1da20b80d28ef33c83faf7af3114a2df60
HabtishB <behabtm@gmail.com>
2022-04-19 08:49:27 -0700

    1-filter states added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
 """
+This script lists all states with a name starting with N(upper N)
+from the database hbtn_0e_0_usa
+It takes 3 arguments username, password, and database name
+It runs on a localhost at port 3306
+results are sorted in ascending order by states.id
 """
 
 import sys
@@ -9,9 +13,11 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                          password=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3], host='localhost', port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states\
+                   WHERE name LIKE BINARY 'N%'\
+                   ORDER BY states.id ASC")
 
     states = cursor.fetchall()
     for state in states:
