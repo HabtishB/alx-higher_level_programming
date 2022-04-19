51ff865fbde126ff1a9ede8921fc994e1a73c9c7
HabtishB <behabtm@gmail.com>
2022-04-19 08:21:29 -0700

    ORM added

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         password=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+This object relational mapping file contains execrices that help to create sql database using
+python via the SQLalchemy ORM
