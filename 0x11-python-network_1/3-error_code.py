85e76c56a0cf23feb49b3f586301fb3fa9663909
HabtishB <behabtm@gmail.com>
2022-04-29 23:17:03 +0000

    3-error code added

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This script takes in a URL and displays the body of the response
+    decoded in utf-8
+    You have to manage the urllib.error.HTPError eexceptions and print
+    the Error code: followed by the HTTP status code
+    The packages urllib and sys should be used
+    You are not allowed to import packages other than urllib and sys
+    Check arguments passed to the script(number or type) is not needed
+    with statement should be used
+"""
+
+if __name__ == "__main__":
+    from urllib import parse
+    from urllib import request, error
+    from sys import argv
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as error:
+        print('Error code:', er.code)
