95f38eae381c5b29bd259e2978b360263d7d1945
HabtishB <behabtm@gmail.com>
2022-04-30 01:37:47 +0000

    4 hbtn status added

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     import requests
 
     res = requests.get("https://intranet.hbtn.io/status")
-    print('Body response')
+    print('Body response:')
     print('\t- type: {}'.format(type(res.text)))
     print('\t- content: {}'.format(res.text))
