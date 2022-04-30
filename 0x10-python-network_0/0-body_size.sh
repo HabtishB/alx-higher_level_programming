aa4c5531eae9a8ef62ca69dcb0293295ce4cc83e
HabtishB <behabtm@gmail.com>
2022-04-30 16:51:26 -0700

    0-added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script that takes in url, and displays the size of the body of the
-# response, in bytes using curl
-
+# Bash script that takes in url, and displays the size of the body of the response
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
